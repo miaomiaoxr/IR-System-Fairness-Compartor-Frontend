7651ba3e7fd5b17a8ed01974cef73a28eec10e0c
miaomiaoxr <miaomiaoxr1234@gmail.com>
2022-07-28 13:22:02 +0100

    Add confirm on delete button

--- src/components/Delete/DeleteButton.js
+++ src/components/Delete/DeleteButton.js
@@ -3,12 +3,13 @@ import { Button } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import NetWork from '../../services/Network'
 
-const DeleteButton = ({ modelID,setData }) => {
+const DeleteButton = ({ modelID, setData }) => {
 
     const handleClick = () => {
-        setData(pre => pre.filter(model => model.id !== modelID))//delete in page
-        NetWork.remove(modelID);//delete in DB,should await?
-        console.log(`Deleting ${modelID}`)
+        if (window.confirm('Are you sure you want to delete this item?')) {
+            setData(pre => pre.filter(model => model.id !== modelID))//delete in page
+            NetWork.remove(modelID);//delete in DB,should await?
+        }
     }
 
     return (
