bec7badf9a871951bef2b4a8d492633700f39c90
miaomiaoxr <miaomiaoxr1234@gmail.com>
2022-08-11 19:06:35 +0100

    Fix pyEval

--- src/components/BackDrop/Chart/ChartAndForms/Form/PyEvals.js
+++ src/components/BackDrop/Chart/ChartAndForms/Form/PyEvals.js
@@ -1,17 +1,10 @@
 import { Typography } from "@mui/material"
 
-const PyEvals = ({ pyEval, showQids }) => {
+const PyEvals = ({ pyEval }) => {
     if (!pyEval) return <></>;
     const ret = [];
     for (let title in pyEval) {
-        ret.push(<br key={title + 'br'} />);
-
-        ret.push(<Typography key={title} variant="h7" sx={{ fontWeight: 600 }}>{title}: </Typography>);
-        for (let v in pyEval[title]) {
-            if (showQids.includes(v))
-                ret.push(<Typography key={v + title} variant="body1">{`${v}: ${pyEval[title][v]}`}</Typography>);
-        }
-
+        ret.push(<Typography key={title} variant="body1">{`${title}: ${pyEval[title]}`}</Typography>);//
     }
     return ret;
 }
